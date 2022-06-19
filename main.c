ed2464ebd3ada9abca5f3d6c5866a8ebca4250ca
CatsArePanzers <kristianm2005@gmail.com>
2022-06-19 12:52:40 +0300

    added transposition of a matrix

--- main.c
+++ main.c
@@ -4,12 +4,17 @@
 #include "file_operations.h"
 
 int main() {
-    matrix_t *matrix = matrix_init(3, 3, "matrix1.txt");;
+    int count = 1;
+
+    matrix_t *matrix = matrix_init(3, 4, "matrix1.txt");
+
     for(int i = 0; i < matrix->rows; i++){
         for(int j = 0; j < matrix->columns; j++){
-            matrix->elements[i][j] = 1;
+            matrix->elements[i][j] = count++;
         }
     }
+
+    matrix_transpose(matrix);
 
     write_matrix(matrix);
 
--- matrix.h
+++ matrix.h
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+void swap(int *a, int* b) {
+    int c = *a;
+    *a = *b;
+    *b = c;
+}
+
 typedef struct {
     int rows;
     int columns;
@@ -9,7 +15,7 @@ typedef struct {
     char *alias;
 }matrix_t;
 
-matrix_t* matrix_init(int rows, int columns, char *name) {
+matrix_t *matrix_init(int rows, int columns, char *name) {
     matrix_t *new_matrix = malloc(sizeof(matrix_t *));
 
     new_matrix->rows = rows;
@@ -26,3 +32,41 @@ matrix_t* matrix_init(int rows, int columns, char *name) {
     return new_matrix;
 }
 
+void matrix_copy(matrix_t *dest, matrix_t *src) {
+    strcpy(dest->alias, src->alias);
+    dest->rows = src->rows;
+    dest->columns = src->columns;
+
+    dest->elements = realloc(dest->elements, sizeof(int *) * src->rows);
+
+    for (int i = 0; i < dest->rows; i++) 
+        dest->elements[i] = malloc(sizeof(int) * dest->columns);
+
+    for (int i = 0; i < dest->rows; i++)
+        for (int j = 0; j < dest->columns; j++) 
+            dest->elements[i][j] = src->elements[i][j];
+}
+
+void transpose_symetrical(matrix_t *matrix) {
+    for (int i = 0; i < matrix->rows; i++) 
+        for (int j = i; j < matrix->columns; j++)
+            swap(&matrix->elements[i][j], &matrix->elements[j][i]);
+}
+
+void transpose_unsymetrical(matrix_t *matrix) {
+    matrix_t *transposed = matrix_init(matrix->columns, matrix->rows, matrix->alias);
+
+    for (int i = 0; i < transposed->rows; i++)
+        for (int j = 0; j < transposed->columns; j++)
+            transposed->elements[i][j] = matrix->elements[j][i];
+
+    matrix_copy(matrix, transposed);
+    
+    free(transposed);
+}
+
+void matrix_transpose(matrix_t *matrix) {
+    if (matrix->rows == matrix->columns)
+        transpose_symetrical(matrix);
+    else transpose_unsymetrical(matrix);
+}
